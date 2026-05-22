88d5e9484a51c73021d15784cf40849ca824cff9
Alan-Rico <oscaralan1806@gmail.com>
2026-05-22 11:23:56 -0600

    First commit

---
+++ src/test/java/com/platzi/demo_market/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.demo_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
